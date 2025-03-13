19e390e7b117257342565ae2c466b02a744c575b
CreatorWassim <wouledmed@gmail.com>
2025-03-13 08:26:25 -0400

    Update application.cpp

--- Application/Script/Application.cpp
+++ Application/Script/Application.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	Window window(1280, 720, "Silver Clad");
 	Renderer renderer;
-
+	// Importing OBJ
 	Entity entity1(1);
 	Entity entity2(2);
 	renderer.LoadModel("Assets/3D_Assets/cottageHouse.obj", entity1, { { 0, 0, 0 }, { 0, 0, 0 }, { 1, 1, 1 } });
